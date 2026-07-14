ad4b32982362277d23447ee08c1d17601a793c8e
Subiyan <subiyanr813@gmail.com>
2026-07-14 16:15:38 +0530

    improved authentication UI and validation flow

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -5,7 +5,6 @@ import { toast } from 'react-toastify';
 import { useAuth } from '../context/AuthContext';
 import { loginSchema, getZodErrors } from '../utils/validationSchemas';
 import FormInput from '../components/FormInput';
-import PasswordInput from '../components/PasswordInput';
 
 const Login = () => {
   const [email, setEmail] = useState('');
@@ -82,7 +81,9 @@ const Login = () => {
           touched={touched.email}
         />
 
-        <PasswordInput
+        <FormInput
+          type="password"
+          placeholder="Password"
           value={password}
           onChange={(e) => handleChange('password', e.target.value, setPassword)}
           error={errors.password}
